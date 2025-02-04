1e1e19a1342416209574f72f354f6eff8ab096d9
Umair Bin Nasir <un315328@gmail.com>
2025-02-04 03:02:17 -0800

    updated

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://shop-now-by-umair.vercel.app/stripe/success"
-      cancelUrl="https://shop-now-by-umair.vercel.app/stripe/error"
+      successUrl="https://umairportfolio-xi.vercel.app/stripe/success"
+      cancelUrl="https://umairportfolio-xi.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
